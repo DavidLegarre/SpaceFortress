eef4c9bd57a87d0ab0ea420d53589c14e18d2447
DavidLegarre <davidls920@gmail.com>
2024-03-11 18:25:34 +0100

    Added galaxy class

--- build/src/main.cpp.d
+++ build/src/main.cpp.d
@@ -1,2 +1,5 @@
-build/./src/main.cpp.o: src/main.cpp include/Generators/GalaxyMap.h
+build/./src/main.cpp.o: src/main.cpp include/Generators/GalaxyMap.h \
+ include/Graphs/AstralBody/Galaxy.h include/Graphs/Node.h
 include/Generators/GalaxyMap.h:
+include/Graphs/AstralBody/Galaxy.h:
+include/Graphs/Node.h:
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 #include "Generators/GalaxyMap.h"
+#include "Graphs/AstralBody/Galaxy.h"
 
 int main() {
     GalaxyMap galaxy;
     galaxy.printMap();
+    Galaxy galaxyTest = Galaxy("Andromeda", 4);
+    galaxyTest.printDetails();
     return 0;
 }
